73fc08a0ecfcb465e9f700dff28f1de0f7897bcf
Mathieu Débit <mathieu.debit@gmail.com>
2017-03-06 22:09:08 +0100

    Implement DJ app project example
    Implement the SuperpoweredCrossExample with two audio files and some effects.

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,8 @@ import {
   StyleSheet,
   Text,
   View,
+  Button,
+  Slider,
   NativeModules
 } from 'react-native';
 
@@ -18,30 +20,85 @@ export default class ReactNativeSuperpowered extends Component {
     super(props);
 
     this.state = {
-      volume: null,
+      init: null,
+      playPause: false,
+      buttonTitle: 'Play',
     }
+
+    this.playPause = this.playPause.bind(this);
   }
 
   componentDidMount() {
-    this.getVolume();
+    this.init();
   }
 
-  async getVolume() {
+  async init() {
     try {
-      let volume = await SuperpoweredModule.getVolume();
+      let init = await SuperpoweredModule.init();
 
-      this.setState({ volume });
+      this.setState({ init });
     } catch (e) {
       console.error(e);
     }
   }
 
+  async playPause() {
+    try {
+      let playPause = await SuperpoweredModule.playPause();
+      let buttonTitle = 'Play';
+
+      if (this.state.buttonTitle == buttonTitle) {
+        buttonTitle = 'Pause';
+      }
+
+      this.setState({ playPause, buttonTitle });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  updateCrossFader(value) {
+    SuperpoweredModule.crossFader(value);
+  }
+
+  updateFxFlanger(value) {
+    SuperpoweredModule.fxValue(0, value);
+  }
+
+  updateFxFilter(value) {
+    SuperpoweredModule.fxValue(1, value);
+  }
+
+  updateFxRoll(value) {
+    SuperpoweredModule.fxValue(2, value);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Volume: { this.state.volume }
-        </Text>
+        {
+          this.state.init == "true" &&
+
+            <View>
+              <Button
+                style={styles.button}
+                onPress={this.playPause}
+                title={this.state.buttonTitle}
+              />
+
+            <Text style={styles.text}>Crossfader</Text>
+              <Slider value={0} onValueChange={this.updateCrossFader} />
+
+              <Text style={styles.text}>Flanger</Text>
+              <Slider value={0} onValueChange={this.updateFxFlanger} />
+
+              <Text style={styles.text}>Filter</Text>
+              <Slider value={0} onValueChange={this.updateFxFilter} />
+
+              <Text style={styles.text}>Roll</Text>
+              <Slider value={0} onValueChange={this.updateFxRoll} />
+            </View>
+        }
       </View>
     );
   }
@@ -50,14 +107,14 @@ export default class ReactNativeSuperpowered extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  text: {
     textAlign: 'center',
     margin: 10,
+    width: 270,
   },
 });
 
